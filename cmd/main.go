97e820dd6ca45490c8a6cb4ddba05c16762a726d
Home <ice_rain2009@mail.ru>
2023-06-28 16:12:19 +0300

    add viper

--- cmd/main.go
+++ cmd/main.go
@@ -6,14 +6,25 @@ import (
 	"rest_api_TODO/pkg/handler"
 	"rest_api_TODO/pkg/repository"
 	"rest_api_TODO/pkg/service"
+
+	"github.com/spf13/viper"
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing configs: %s", err.Error())
+	}
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 	srv := new(restapi.Server)
-	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while renning http server: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
