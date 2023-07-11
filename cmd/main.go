267b94693649446ab90a8e959d2e8c2df4ff8ea9
Home <ice_rain2009@mail.ru>
2023-07-11 10:42:33 +0300

    added and setup logrus

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"log"
 	"os"
 	restapi "rest_api_TODO"
 	"rest_api_TODO/pkg/handler"
@@ -10,17 +9,20 @@ import (
 
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
+	"github.com/sirupsen/logrus"
 
 	"github.com/spf13/viper"
 )
 
 func main() {
+
+	logrus.SetFormatter(new(logrus.JSONFormatter))
 	if err := initConfig(); err != nil {
-		log.Fatalf("error initializing configs: %s", err.Error())
+		logrus.Fatalf("error initializing configs: %s", err.Error())
 	}
 
 	if err := godotenv.Load(); err != nil {
-		log.Fatalf("error loading env variables: %s", err.Error())
+		logrus.Fatalf("error loading env variables: %s", err.Error())
 	}
 
 	db, err := repository.NewPostgresDB(repository.Config{
@@ -32,7 +34,7 @@ func main() {
 		SSLMode:  viper.GetString("db.sslmode"),
 	})
 	if err != nil {
-		log.Fatalf("failed to intialized db: %s", err.Error())
+		logrus.Fatalf("failed to intialized db: %s", err.Error())
 	}
 
 	repos := repository.NewRepository(db)
@@ -40,7 +42,7 @@ func main() {
 	handlers := handler.NewHandler(services)
 	srv := new(restapi.Server)
 	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
-		log.Fatalf("error occured while renning http server: %s", err.Error())
+		logrus.Fatalf("error occured while renning http server: %s", err.Error())
 	}
 }
 
