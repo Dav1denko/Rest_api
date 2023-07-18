3352c72351488055b5c80e99bccc4251414f2833
Home <ice_rain2009@mail.ru>
2023-07-18 16:08:01 +0300

    fix get all items

--- todo.go
+++ todo.go
@@ -18,7 +18,7 @@ type TodoItem struct {
 	Id          int    `json:"id" db:"id"`
 	Title       string `json:"title" db:"title" binding:"required"`
 	Description string `json:"description" db:"description"`
-	Done        bool   `json:"done"db:"done"`
+	Done        bool   `json:"done" db:"done"`
 }
 
 type ListsItem struct {
